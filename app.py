5023c242635eb692b9a82a49c5b57a67e5602942
dStass <dstiasny@outlook.com>
2020-07-30 09:33:44 +1000

    search restructing + step length binary search

--- app.py
+++ app.py
@@ -3,211 +3,20 @@ import random
 import numpy as np
 
 from process_csv.csvrw import CSVReadWrite
-from funcs.functions import Z, P, A, B, C, D
+from funcs.functions import *
 
 # # # # # # # # #
-#  DEFINITIONS  #
+#   VARIABLES   #
 # # # # # # # # #
 
-def build_poly(weight, i):
-  weightstr = str(weight)
-  if i == 0:
-    return weightstr
-  elif i == 1:
-    return weightstr + ' * x'
-  else:
-    return weightstr + ' * x^' + str(i)
+# M (M+1 elements): no. polynomial coefficients, form: p_o + p_1*x + p_2*x^2 + ... + p_M
+# N (N+1 elements): no. sinusoidal coefficients of form SUM(n=0..N){ a_n*cos(b_n*x) + c_n*cos(d_n*x) }
+M = 1
+N = 4
 
-def build_equation(weights, M, N):
-  eqn = ''
-  for i in range(0, M+1):
-    eqn += build_poly(weights[i], i)
-    eqn += ' + '
-
-  for i in range(0, N+1):
-    a_i_pos = (M+1) + i
-    b_i_pos = a_i_pos + (N+1)
-    c_i_pos = b_i_pos + (N+1)
-    d_i_pos = c_i_pos + (N+1)
-    a_i = weights[a_i_pos]
-    b_i = weights[b_i_pos]
-    c_i = weights[c_i_pos]
-    d_i = weights[d_i_pos]
-    eqn += '{} * cos({} * x) + {} * sin({} * x)'.format(a_i, b_i, c_i, d_i)
-    eqn += ' + '
-  
-  eqn = eqn[:-3]
-  return eqn
-
-
-def sqerr(x, y):
-  err = 0
-  for i in range(len(x)):
-    err += pow((x[i] - y[i]), 2)
-  return err/len(x)
-
-def f_vec(vec, weights, weights_mapping, M, N):
-  to_return = []
-  for v in vec:
-    to_return.append(f(v, weights, weights_mapping, M, N))
-  return to_return
-
-def f(x, weights, weights_mapping, M, N):
-  function_val = 0
-  for i in range((M+1)):
-    function_val += weights[i] * pow(x, i)
-  
-  for i in range(N+1):
-    a_i_pos = (M+1) + i
-    b_i_pos = a_i_pos + (N+1)
-    c_i_pos = b_i_pos + (N+1)
-    d_i_pos = c_i_pos + (N+1)
-    a_i = weights[a_i_pos]
-    b_i = weights[b_i_pos]
-    c_i = weights[c_i_pos]
-    d_i = weights[d_i_pos]
-    function_val += a_i * math.cos(b_i * x) + c_i * math.sin(d_i * x)
-  
-  return function_val
-
-def pj(j, data, weights, weights_mapping, S):
-  if j == 0: return 0
-  ongoing_sum = 0
-  for tup in data:
-    x_k = tup[0]
-    y_k = tup[1]
-    p_j = weights[weights_mapping['p_'+str(j)]]
-    S1_k = S['S1_'+str(x_k)]
-    ongoing_sum += 2*j*p_j*(pow(x_k, 2*j-1) + pow(x_k, j-1) * S1_k - y_k * pow(x_k, j-1))
-  return ongoing_sum
-
-def aj(j, data, weights, weights_mapping, S):
-  ongoing_sum = 0
-  for tup in data:
-    x_k = tup[0]
-    y_k = tup[1]
-    b_j = weights[weights_mapping['b_'+str(j)]]
-    S1_k = S['S1_'+str(x_k)]
-    S2_k = S['S2_'+str(x_k)]
-    ongoing_sum += 2*math.cos(b_j*x_k) * (S1_k + S2_k - y_k)
-  return ongoing_sum
-
-def bj(j, data, weights, weights_mapping, S):
-  ongoing_sum = 0
-  for tup in data:
-    x_k = tup[0]
-    y_k = tup[1]
-    a_j = weights[weights_mapping['a_'+str(j)]]
-    b_j = weights[weights_mapping['b_'+str(j)]]
-    S1_k = S['S1_'+str(x_k)]
-    S2_k = S['S2_'+str(x_k)]
-    ongoing_sum += -(2 * x_k * a_j * math.sin(b_j * x_k) * (S1_k + S2_k - y_k))
-  return ongoing_sum
-
-def cj(j, data, weights, weights_mapping, S):
-  ongoing_sum = 0
-  for tup in data:
-    x_k = tup[0]
-    y_k = tup[1]
-    d_j = weights[weights_mapping['d_'+str(j)]]
-    S1_k = S['S1_'+str(x_k)]
-    S2_k = S['S2_'+str(x_k)]
-    ongoing_sum += 2 * math.sin(d_j * x_k) * (S1_k + S2_k - y_k)
-  return ongoing_sum
-
-def dj(j, data, weights, weights_mapping, S):
-  ongoing_sum = 0
-  for tup in data:
-    x_k = tup[0]
-    y_k = tup[1]
-    c_j = weights[weights_mapping['c_'+str(j)]]
-    d_j = weights[weights_mapping['d_'+str(j)]]
-    S1_k = S['S1_'+str(x_k)]
-    S2_k = S['S2_'+str(x_k)]
-    ongoing_sum += 2 * x_k * c_j * math.cos(d_j * x_k) * (S1_k + S2_k - y_k)
-  return ongoing_sum
-
-def calculateS1_at_xk(data, weights, weights_mapping, N, x_k):
-  ongoing_sum = 0
-  for n in range(N):
-    # declare coefficients
-    a_n = weights[weights_mapping['a_' + str(n)]]
-    b_n = weights[weights_mapping['b_' + str(n)]]
-    c_n = weights[weights_mapping['c_' + str(n)]]
-    d_n = weights[weights_mapping['d_' + str(n)]]
-
-    # build the sum
-    ongoing_sum += a_n * math.cos(b_n * x_k) + c_n * math.sin(d_n * x_k)
-  return ongoing_sum
-
-def calculateS2_at_xk(data, weights, weights_mapping, M, x_k):
-  ongoing_sum = 0
-  for m in range(M):
-    # declare coefficients
-    p_m = weights[weights_mapping['p_' + str(m)]]
-
-    # build the sum
-    ongoing_sum += p_m * pow(x_k, m)
-  return ongoing_sum
-
-def calculate_S(data, weights, weights_mapping, N, M):
-  S = {}
-  for tup in data:
-    x_k = tup[0]      
-    key = 'S1_' + str(x_k)
-    S1_k = calculateS1_at_xk(data, weights, weights_mapping, N, x_k)
-    S[key] = S1_k
-
-  for tup in data:
-    x_k = tup[0]      
-    key = 'S2_' + str(x_k)
-    S2_k = calculateS2_at_xk(data, weights, weights_mapping, M, x_k)
-    S[key] = S2_k
-
-  return S
-
-
-
-# vector methods
-
-def mul_scalar_to_vec(scalar, vec):
-  return [scalar * v for v in vec]
-
-def add_vectors(vec0, vec1):
-  to_return = []
-  for i in range(len(vec0)):
-    to_return.append(vec0[i] + vec1[i])
-  return to_return
-
-def normalise_vector(vec0):
-  vec_norm = norm_euclidean(vec0)
-  to_return = []
-  for v in vec0:
-    to_return.append(v/vec_norm)
-  return to_return
-
-def neg_vector(vec):
-  return [-v for v in vec]
-
-def get_gradient_at(gradient, weights, weights_mapping, data, S, N, M):
-  evaluated_gradient = []
-  total = 0
-  for i in range(len(gradient)):
-    j = i
-    if total >= M:
-      j = (i - M) % N
-
-    g_i_function = gradient[i]
-    g_i_eval = g_i_function(j, data, weights, weights_mapping, S)
-    evaluated_gradient.append(g_i_eval)
-    total += 1
-  return evaluated_gradient
-
-def norm_euclidean(weights):
-  ongoing = 0
-  for val in weights:
-    ongoing += pow(val, 2)
-  return math.sqrt(ongoing)
+# assume data has been split
+# paths
+training_path = 'training.csv'
 
 
 # # # # # # # # #
@@ -221,44 +30,32 @@ print("Starting app..")
 # PREPROCESSING #
 # # # # # # # # #
 
-# assume data has been split
-# paths
-training_path = 'training2.csv'
-
 # read training data
 csvrw = CSVReadWrite()
 training_data = csvrw.csv_to_list(training_path)
 training_data = [(float(tup[0]), float(tup[1])) for tup in training_data]
+
+# separate data into x and y lists
 x = [tup[0] for tup in training_data]
 y = [tup[1] for tup in training_data]
 
+# transform y
+y = [s - (47.222 + 0.0003*t) for s, t in zip(y, x)]
+data = [(x[i], y[i]) for i in range(len(x))]
+
 # polynomial + sinusoidal function estimator with gradient descent
-# set up variables:
-variables = {}
-
-# set up memoized
-memoized = {}
-for k in ['P', 'A', 'B', 'C', 'D']:
-  memoized[k] = {}
-
-# M (M+1 elements): no. polynomial coefficients, form: p_o + p_1*x + p_2*x^2 + ... + p_M
-# N (N+1 elements): no. sinusoidal coefficients of form SUM(n=0..N){ a_n*cos(b_n*x) + c_n*cos(d_n*x) }
-M = 1
-N = 3
-
 # function: SUM(m=0..M){ p_m * x^m } + SUM(n=0..N){ a_n*cos(b_n*x) + c_n*cos(d_n*x) }
-# gradient, first M+1 elements are gradients of polynomial terms
-gradient = []  # gradient vector
-variable_name_to_position = {}  # maps a variable name to its position in x and g (for its partial differentiation)
-total = 0
-
-
 # estimates, total size = ((M+1) + (N+1)*4)
+# gradient vector:
 # first M+1 elements are assigned to p_m in increasing order
 # next N+1 elements are assigned to a_n in increasing order
 # next N+1 elements are assigned to b_n in increasing order
 # next N+1 elements are assigned to c_n in increasing order
 # next N+1 elements are assigned to d_n in increasing order
+gradient = []  # gradient vector
+variable_name_to_position = {}  # maps a variable name to its position in x and g (for its partial differentiation)
+total = 0
+
 
 # fill variable_name_to_position
 # add polynomial coefficients
@@ -273,102 +70,151 @@ for char in ['a','b','c','d']:
   for n in range(0, N+1):
     a_n_name = char + '_' + str(n)
     variable_name_to_position[a_n_name] = total
-    gradient.append(globals()[char+'j'])
+    gradient.append(globals()[char+'j'])  # i.e. add methods aj, bj, cj, dj
     total += 1
-
-MIN_SQERR = 0.2
-MIN_NORM = 2000
-sqerror = MIN_SQERR + 1
-norm = MIN_NORM + 1
-
-
-while sqerror > MIN_SQERR or norm > MIN_NORM:
-  weights = [random.uniform(-0.5,0.5) for _ in range(((M+1) + 4*(N+1)))]
-  weights[0] = 0
-  weights[1] = 0
-  # weights[0] = 47.22
-  # weights[1] = 0.0003
-  # weights[2] = 0
-  fvec = f_vec(x, weights, variable_name_to_position, M, N)
-  sqerror = sqerr(fvec, y)
-
-  S = calculate_S(training_data, weights, variable_name_to_position, N, M)
-  norm = norm_euclidean(get_gradient_at(gradient, weights, variable_name_to_position, training_data, S, N, M))
-  print()
-
-
-
 
 # # # # # # # # #
 #    LEARNING   #
 # # # # # # # # #
 
-EPS = 0.0005
-rate = 0.000001
-MAX_STEPS = 10000
+EPS = 0.00000000005
+RATE = 1.0
+THRESHOLD_SQ_ERR = 0.002
+INTERVAL_SPLIT = 128
 step_count = 0
 total = 0
+MODELS_TO_COLLECT = 5
 
-norms = []
-running_norm = 0
-min_weights = None
-min_sqerr = float('inf')
+models = {}
 
-print("begin")
+# running_norm = 0
+# min_weights = None
+# min_sqerr = float('inf')
+# prev_running_norm = float('inf')
+prev_running_norm = float('inf')
+prev_weights = None
 
-# while step_count < MAX_STEPS:
-while True:
-  # if step_count == MAX_STEPS:
-  #   weights = [random.uniform(0, 1) for _ in range(((M+1) + 4*(N+1)))]
+print("Begin descent")
 
-  S = calculate_S(training_data, weights, variable_name_to_position, N, M)
-  step = neg_vector(get_gradient_at(gradient, weights, variable_name_to_position, training_data, S, N, M))
-  # step = get_gradient_at(gradient, weights, variable_name_to_position, training_data, S, N, M)
-  # step = normalise_vector(step)
-  weights = add_vectors(weights, mul_scalar_to_vec(rate, step))
-  norm = norm_euclidean(get_gradient_at(gradient, weights, variable_name_to_position, training_data, S, N, M))
-  # norms.append((str(step_count), str(norm)))
-  running_norm += norm
-  fvec = f_vec(x, weights, variable_name_to_position, M, N)
-  sqerror = sqerr(fvec, y)
-  if total > 0 and total % 50 == 0:
-    print("sqerr = ", sqerror)
-    print("avgnorm = ", running_norm / total)
+
+interval = 0
+while interval + INTERVAL_SPLIT < len(x):
+  models[interval] = []
+
+  # gather subset of data
+  x_subset = x[interval : interval + INTERVAL_SPLIT]
+  y_subset = y[interval : interval + INTERVAL_SPLIT]
+  data_subset = [(x_subset[i], y_subset[i]) for i in range(len(x_subset))]
+
+  # initialise weights vector
+  weights = generate_weights(data, gradient, variable_name_to_position, M, N)
+
+  prev_running_norm = 0
+  running_norm = 0
+
+  its = 0
+  sq_err = float('inf')
+  while True:
+    its += 1
+    # get squared sums and generate step/descent direction
+    S = calculate_S(x, weights, variable_name_to_position, N, M)
+    descent_direction = neg_vector(get_gradient_at(gradient, weights, variable_name_to_position, data_subset, S, N, M))
+    descent_direction = normalise_vector(descent_direction)
+
+    # build step
+    rate = RATE
+    step = 0
+    while True:
+      new_step = step + rate
+      rate /= 2
+      new_weights = add_vectors(weights, mul_scalar_to_vec(new_step, descent_direction))
+      f_x = apply_function(x_subset, new_weights, variable_name_to_position, M, N)
+
+      # get squared error
+      new_sq_err = get_square_err(f_x, y_subset)
+      if new_sq_err <= sq_err:
+        sq_err = new_sq_err
+        step = new_step
+    
+      if rate <= EPS: break
+    
+    print(sq_err, rate)
+    if step == 0:
+      weights = generate_weights(data_subset, gradient, variable_name_to_position, M, N)
+      running_norm = 0
+      prev_running_norm = 0
+      sq_err = float('inf')
+      continue
+    weights = add_vectors(weights, mul_scalar_to_vec(step, descent_direction))
+    norm = norm_euclidean(get_gradient_at(gradient, weights, variable_name_to_position, data_subset, S, N, M))
+    running_norm += norm
+    if sq_err < THRESHOLD_SQ_ERR:
+      if running_norm < prev_running_norm:
+        prev_running_norm = running_norm
+        prev_weights = weights
+      else:
+        models[interval].append([w for w in prev_weights])
+        eqn = build_equation(weights, M, N)
+        text = "cycle= " + str(total) + " min_sqerr = " + str(min_sqerr) + ", eqn=" + eqn + '\n\n\n'
+        print("sqerr=", sqerror, ", eqn = ", eqn)
+        if len(models[interval]) > MODELS_TO_COLLECT: break
+    else:
+      if its % 100 == 0 and running_norm > prev_running_norm:
+        weights = generate_weights(data_subset, gradient, variable_name_to_position, M, N)
+        sq_err = float('inf')
+        running_norm = 0
+        prev_running_norm = 0
+        continue
+
+  interval += 1
+  
+
+
+
+# while True:
+
+#   S = calculate_S(x, weights, variable_name_to_position, N, M)
+#   step = neg_vector(get_gradient_at(gradient, weights, variable_name_to_position, data, S, N, M))
+#   step = normalise_vector(step)
+#   local_rate = rate
+#   while True:
+#     # step = get_gradient_at(gradient, weights, variable_name_to_position, training_data, S, N, M)
+#     new_weights = add_vectors(weights, mul_scalar_to_vec(local_rate, step))
+    
+#     # norms.append((str(step_count), str(norm)))
+#     fvec = apply_function(x, new_weights, variable_name_to_position, M, N)
+#     new_sqerror = sqerr(fvec, y)
+#     if new_sqerror < sqerror:
+#       weights = new_weights
+#       sqerror = new_sqerror
+#       break
+#     local_rate /= 2
+#     if local_rate == 0:
+#       weights = weights = generate_weights(data, gradient, variable_name_to_position, M, N)
+#       break
+
+
+#   norm = norm_euclidean(get_gradient_at(gradient, weights, variable_name_to_position, data, S, N, M))
+#   running_norm += norm
+#   if total > 0 and total % 50 == 0:
+#     print("sqerr = ", sqerror)
+#     print("avgnorm = ", running_norm / total)
+#     if running_norm < prev_running_norm:
+#       prev_running_norm = running_norm
+#     else:
+#       weights = weights = generate_weights(data, gradient, variable_name_to_position, M, N)
 
   
-  if sqerror < min_sqerr:
-    # min_norm = norm
-    min_sqerr = sqerror
-    min_weights = [w for w in weights]
-    text = "cycle= " + str(total) + " min_sqerr = " + str(min_sqerr) + ", eqn=" + build_equation(weights, M, N) + '\n\n\n'
-    with open("log.txt", "a") as myfile:
-      myfile.write(text)
-    step_count = 0
-  # print("avg=", sum(norms)/len(norms), "min=", min(norms))
-  step_count += 1
-  total += 1
-
-
-# save_path = 'save_output.csv'
-# csvrw.list_to_csv(norms, save_path)
-# print(norms)
-
-
-
-# print(get_g_at_x(g, x))
-# print(norm_euclidean(get_g_at_x(g, x)))
-# while (norm_euclidean(get_gradient_at(gradient, weights)) > eps):
-#   if step > MAX_STEPS: break
-#   step = neg_vector(get_gradient_at(g, x))
-#   x = add_vectors(x, mul_scalar_to_vec(rate, s))
-  
-#   for k in variable_name_to_position:
-#     position = variable_name_to_position[k]
-#     variables[k] = x[position]
-  
-#   step += 1
-
-#   norm = norm_euclidean(get_g_at_x(g, x))
-
-
-# print("norm:", norm, "x=\n", x)
+#   if sqerror < min_sqerr or sqerror < THRESHOLD_SQERR:
+#     # min_norm = norm
+#     min_sqerr = min(min_sqerr, sqerror)
+#     min_weights = [w for w in weights]
+#     eqn = build_equation(weights, M, N)
+#     text = "cycle= " + str(total) + " min_sqerr = " + str(min_sqerr) + ", eqn=" + eqn + '\n\n\n'
+#     print("sqerr=", sqerror, ", eqn = ", eqn)
+#     with open("log.txt", "a") as myfile:
+#       myfile.write(text)
+#     step_count = 0
+#   # print("avg=", sum(norms)/len(norms), "min=", min(norms))
+#   step_count += 1
+#   total += 1
--- funcs/functions.py
+++ funcs/functions.py
@@ -1,132 +1,245 @@
 import math
-class Function:
-  def __init__(self, variables, memoized):
-    self.variables = variables
-    self.memoized = memoized
-    pass
+import random
 
-  def execute(self, x):
-    pass
+def build_equation(weights, M, N):
+  def build_poly(weight, i):
+    weightstr = str(weight)
+    if i == 0:
+      return weightstr
+    elif i == 1:
+      return weightstr + ' * x'
+    else:
+      return weightstr + ' * x^' + str(i)
 
-class Z(Function):
-  """
-  returns zero
-  """
-  def execute(self, x=None):
-    return 0
+  eqn = ''
+
+  for i in range(0, M+1):
+    eqn += build_poly(weights[i], i)
+    eqn += ' + '
+
+  for i in range(0, N+1):
+    a_i_pos = (M+1) + i
+    b_i_pos = a_i_pos + (N+1)
+    c_i_pos = b_i_pos + (N+1)
+    d_i_pos = c_i_pos + (N+1)
+    a_i = weights[a_i_pos]
+    b_i = weights[b_i_pos]
+    c_i = weights[c_i_pos]
+    d_i = weights[d_i_pos]
+    eqn += '{} * cos({} * x) + {} * sin({} * x)'.format(a_i, b_i, c_i, d_i)
+    eqn += ' + '
   
-class P(Function):
-  """
-  returns val * p_m * x^p
-  """
+  eqn = eqn[:-3]
+  return eqn
 
-  def __init__(self, variables, memoized, p_m_name, val, p):
-    self.variables = variables
-    self.memoized = memoized
-    self.p_m_name = p_m_name
-    self.val = val
-    self.p = p
+def get_square_err(x, y):
+  err = 0
+  for i in range(len(x)):
+    err += pow((x[i] - y[i]), 2)
+  return err/len(x)
 
-  def execute(self, x):
-    p_m = self.variables[self.p_m_name]
-    val = self.val
-    p = self.p
-
-    memoized = self.memoized['P']
-    key = (p_m, val, p)
-    
-    if key not in memoized:
-      memoized[key] = val * p_m * pow(x,p)
-    return memoized[key]
+# execute function
+def apply_function(vec, weights, weights_mapping, M, N):
+  to_return = []
+  for v in vec:
+    to_return.append(f(v, weights, weights_mapping, M, N))
+  return to_return
 
 
-class A(Function):
-  """
-  returns cos(b_n * x)
-  """
+# vector methods
 
-  def __init__(self, variables, memoized, b_n_name):
-    self.variables = variables
-    self.memoized = memoized
-    self.b_n_name = b_n_name
+def mul_scalar_to_vec(scalar, vec):
+  return [scalar * v for v in vec]
 
-  def execute(self, x):
-    b_n = self.variables[self.b_n_name]
-    memoized = self.memoized['A']
-    key = (x,b_n)
-    if key not in memoized:
-      memoized[key] = math.cos(b_n*x)
-    return memoized[key]
+def add_vectors(vec0, vec1):
+  to_return = []
+  for i in range(len(vec0)):
+    to_return.append(vec0[i] + vec1[i])
+  return to_return
+
+def normalise_vector(vec0):
+  vec_norm = norm_euclidean(vec0)
+  to_return = []
+  for v in vec0:
+    to_return.append(v/vec_norm)
+  return to_return
+
+def neg_vector(vec):
+  return [-v for v in vec]
+
+def get_gradient_at(gradient, weights, weights_mapping, data, S, N, M):
+  evaluated_gradient = []
+  total = 0
+  for i in range(len(gradient)):
+    j = i
+    if total >= M:
+      j = (i - M) % N
+
+    g_i_function = gradient[i]
+    g_i_eval = g_i_function(j, data, weights, weights_mapping, S)
+    evaluated_gradient.append(g_i_eval)
+    total += 1
+  return evaluated_gradient
+
+def norm_euclidean(weights):
+  ongoing = 0
+  for val in weights:
+    ongoing += pow(val, 2)
+  return math.sqrt(ongoing)
+
+
+# # # # # # # # # # # #
+#  function specific  #
+# # # # # # # # # # # #
+
+def f(x, weights, weights_mapping, M, N):
+  function_val = 0
+
+  # sum polynomials
+  for i in range((M+1)):
+    function_val += weights[i] * pow(x, i)
   
-class B(Function):
-  """
-  returns - a_n * b_n * sin(b_n * x)
-  """
+  # sum sinusoidal
+  for i in range(N+1):
+    a_i_pos = (M+1) + i
+    b_i_pos = a_i_pos + (N+1)
+    c_i_pos = b_i_pos + (N+1)
+    d_i_pos = c_i_pos + (N+1)
+    a_i = weights[a_i_pos]
+    b_i = weights[b_i_pos]
+    c_i = weights[c_i_pos]
+    d_i = weights[d_i_pos]
+    function_val += a_i * math.cos(b_i * x) + c_i * math.sin(d_i * x)
 
-  def __init__(self, variables, memoized, a_n_name, b_n_name):
-    self.variables = variables
-    self.memoized = memoized
-    self.a_n_name = a_n_name
-    self.b_n_name = b_n_name
-
-  def execute(self, x):
-    a_n = self.variables[self.a_n_name]
-    b_n = self.variables[self.b_n_name]
-    memoized = self.memoized['B']
-    key = (x,a_n,b_n)
-    if key not in memoized:
-      memoized[key] = - a_n * b_n * math.sin(b_n * x)
-    return memoized[key]
+  return function_val
 
 
-class C(Function):
-  """
-  returns sin(d_n * x)
-  """
+# # # # # # # # # # # #
+#       PARTIALS      #
+# # # # # # # # # # # #
 
-  def __init__(self, variables, memoized, d_n_name):
-    self.variables = variables
-    self.memoized = memoized
-    self.d_n_name = d_n_name
+# POLYNOMIAL PARTIALS
+def pj(j, data, weights, weights_mapping, S):
+  if j == 0: return 0
+  ongoing_sum = 0
+  for tup in data:
+    x_k = tup[0]
+    y_k = tup[1]
+    p_j = weights[weights_mapping['p_'+str(j)]]
+    S1_k = S['S1_'+str(x_k)]
+    ongoing_sum += 2*j*p_j*(pow(x_k, 2*j-1) + pow(x_k, j-1) * S1_k - y_k * pow(x_k, j-1))
+  return ongoing_sum
 
-  def execute(self, x):
-    d_n = self.variables[self.d_n_name]
-    memoized = self.memoized['C']
-    key = (x,d_n)
-    if key not in memoized:
-      memoized[key] = math.sin(d_n*x)
-    return memoized[key]
+# SINUSOIDAL PARTIALS
+def aj(j, data, weights, weights_mapping, S):
+  ongoing_sum = 0
+  for tup in data:
+    x_k = tup[0]
+    y_k = tup[1]
+    b_j = weights[weights_mapping['b_'+str(j)]]
+    S1_k = S['S1_'+str(x_k)]
+    S2_k = S['S2_'+str(x_k)]
+    ongoing_sum += 2*math.cos(b_j*x_k) * (S1_k + S2_k - y_k)
+  return ongoing_sum
 
-class D(Function):
-  """
-  returns: c_n * d_n * cos(d_n * x)
-  """
+def bj(j, data, weights, weights_mapping, S):
+  ongoing_sum = 0
+  for tup in data:
+    x_k = tup[0]
+    y_k = tup[1]
+    a_j = weights[weights_mapping['a_'+str(j)]]
+    b_j = weights[weights_mapping['b_'+str(j)]]
+    S1_k = S['S1_'+str(x_k)]
+    S2_k = S['S2_'+str(x_k)]
+    ongoing_sum += -(2 * x_k * a_j * math.sin(b_j * x_k) * (S1_k + S2_k - y_k))
+  return ongoing_sum
 
-  def __init__(self, variables, memoized, c_n_name, d_n_name):
-    self.variables = variables
-    self.memoized = memoized
-    self.c_n_name = c_n_name
-    self.d_n_name = d_n_name
+def cj(j, data, weights, weights_mapping, S):
+  ongoing_sum = 0
+  for tup in data:
+    x_k = tup[0]
+    y_k = tup[1]
+    d_j = weights[weights_mapping['d_'+str(j)]]
+    S1_k = S['S1_'+str(x_k)]
+    S2_k = S['S2_'+str(x_k)]
+    ongoing_sum += 2 * math.sin(d_j * x_k) * (S1_k + S2_k - y_k)
+  return ongoing_sum
 
-  def execute(self, x):
-    c_n = self.variables[self.c_n_name]
-    d_n = self.variables[self.d_n_name]
-    memoized = self.memoized['D']
-    key = (x,c_n,d_n)
-    if key not in memoized:
-      memoized[key] = c_n * d_n * math.cos(d_n * x)
-    return memoized[key]
+def dj(j, data, weights, weights_mapping, S):
+  ongoing_sum = 0
+  for tup in data:
+    x_k = tup[0]
+    y_k = tup[1]
+    c_j = weights[weights_mapping['c_'+str(j)]]
+    d_j = weights[weights_mapping['d_'+str(j)]]
+    S1_k = S['S1_'+str(x_k)]
+    S2_k = S['S2_'+str(x_k)]
+    ongoing_sum += 2 * x_k * c_j * math.cos(d_j * x_k) * (S1_k + S2_k - y_k)
+  return ongoing_sum
 
-# memoized = {
-#   "P" : {},
-#   "A" : {},
-#   "B" : {}
-# }
+# calculate sums S1 and S2 evaluated at data
+def calculateS1_at_xk(weights, weights_mapping, N, x_k):
+  ongoing_sum = 0
+  for n in range(N):
+    # declare coefficients
+    a_n = weights[weights_mapping['a_' + str(n)]]
+    b_n = weights[weights_mapping['b_' + str(n)]]
+    c_n = weights[weights_mapping['c_' + str(n)]]
+    d_n = weights[weights_mapping['d_' + str(n)]]
 
-# variables = {
-#   "a_0" : 1,
-#   "b_0" : 1,
-# }
+    # build the sum
+    ongoing_sum += a_n * math.cos(b_n * x_k) + c_n * math.sin(d_n * x_k)
+  return ongoing_sum
 
-# b = B(variables, memoized, 'a_0', 'b_0')
-# print(b.execute(2))
+def calculateS2_at_xk(weights, weights_mapping, M, x_k):
+  ongoing_sum = 0
+  for m in range(M):
+    # declare coefficients
+    p_m = weights[weights_mapping['p_' + str(m)]]
+
+    # build the sum
+    ongoing_sum += p_m * pow(x_k, m)
+  return ongoing_sum
+
+def calculate_S(x, weights, weights_mapping, N, M):
+  S = {}
+  for x_k in x:
+    key1 = 'S1_' + str(x_k)
+    S1_k = calculateS1_at_xk(weights, weights_mapping, N, x_k)
+    S[key1] = S1_k
+
+    key2 = 'S2_' + str(x_k)
+    S2_k = calculateS2_at_xk(weights, weights_mapping, M, x_k)
+    S[key2] = S2_k
+
+  return S
+
+
+def generate_weights(data, gradient, variable_name_to_position, M, N):
+  MIN_SQERR = 0.2
+  MIN_NORM = 500
+  sqerror = MIN_SQERR + 1
+  norm = MIN_NORM + 1
+
+  x = []
+  y = []
+  for tup in data:
+    x.append(tup[0])
+    y.append(tup[1])
+
+  weights = None
+  # generate a close enough point to start
+  while sqerror > MIN_SQERR or norm > MIN_NORM:
+    weights = [random.uniform(-0.1,0.1) for _ in range(((M+1) + 4*(N+1)))]
+    weights[0] = 0
+    weights[1] = 0
+    # weights[0] = 47.22
+    # weights[1] = 0.0003
+    # weights[2] = 0
+    fvec = apply_function(x, weights, variable_name_to_position, M, N)
+    sqerror = get_square_err(fvec, y)
+
+    S = calculate_S(x, weights, variable_name_to_position, N, M)
+    norm = norm_euclidean(get_gradient_at(gradient, weights, variable_name_to_position, data, S, N, M))
+  
+  return weights
